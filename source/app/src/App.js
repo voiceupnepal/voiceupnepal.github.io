05918410c83357d9d8da8d5418a7bc11c4b5a68e
Aashish Karki <aashish.y2z@gmail.com>
2020-06-10 00:15:51 +0545

    initial setup

---
+++ source/app/src/App.js
@@ -0,0 +1,15 @@
+/* global window document */
+// @flow
+import React, { useState, useCallback } from 'react';
+import './app.scss';
+
+
+function App() {
+
+  return (
+    <div className="container">
+    </div>
+  );
+}
+
+export default App;
